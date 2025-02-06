ddbb060c276fc83d2ac2eb05d50a62e015f16228
electricplayer <40640596+electricplayer@users.noreply.github.com>
2025-02-05 20:54:39 -0600

    contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { PathfindingProvider } from "./context/PathfindingContext";
+import { SpeedProvider } from "./context/SpeedContext";
+import { TileProvider } from "./context/TileContext";
 
 function App() {
   
   return (
     <PathfindingProvider>
-  <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-900">Hello World!</h1>
+    <TileProvider>
+      <SpeedProvider>
+  <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-950">Hello World!</h1>
+    </SpeedProvider>
+  </TileProvider>
   </PathfindingProvider>
   );
 }
